e74d2be891c2fa3bc5933f37744b764566e03435
1293485767 <1293485767@qq.com>
2015-12-20 17:54:01 +0800

    init

---
+++ redis-session-manager/src/main/java/com/orangefunction/tomcat/redissessions/DeserializedSessionContainer.java
@@ -0,0 +1,11 @@
+package com.orangefunction.tomcat.redissessions;
+
+class DeserializedSessionContainer {
+	public final RedisSession session;
+	public final SessionSerializationMetadata metadata;
+
+	public DeserializedSessionContainer(RedisSession session, SessionSerializationMetadata metadata) {
+		this.session = session;
+		this.metadata = metadata;
+	}
+}
